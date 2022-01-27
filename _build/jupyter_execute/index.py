a6673d6ba5f7ca8d4647a86d868ebeaf4bfd98b3
git@ryanwarnock.me <lebronse@mb.local>
2022-01-27 16:41:41 +0800

    initial commit

---
+++ _build/jupyter_execute/index.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # About me
+# 
+# I'm just a guy who loves solving puzzles and searching for patterns which led me to studying and soon to be working in the field of cyber security analysis. I'm always looking for more data to help uncover trends and gain knowledge about my environment so that I can help protect it.
+# 
+# I'm also an avid amateur sysadmin with my own homelab where I self host a number of services used by friends and family.
+# 
+# If you've got a role you think I may be good for get in touch (hello@ryanwarnock.me).
+# 
+# ## Professional and academic accomplishments
+# 
+# - High distinction grade average
+# - Selected for 2022 Australian Defence Force Cyber Gap Program
+# - Edith Cowan Universtity peer mentor
